9e7530727bc0e7b4a7f47fa15ca8c2570cdb624b
BeeTillman <bpt3creations@gmail.com>
2023-11-10 03:01:37 -0500

    Added navbar.jsx/css

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import navbar from '//components/navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <navbar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
