0d47a73f7f1e43894ba578f3c8636d03604d98f6
BeeTillman <bpt3creations@gmail.com>
2023-11-12 03:24:33 -0500

    Modified navbar
    / Fixed Temporary Test Button from Immediately changing visibility
    + Added function to close 'Mobile Menu' when Logo/Name button is clicked

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 import { Button } from './Button';
@@ -19,13 +19,17 @@ function Navbar() {
     }
   };
 
+  useEffect(() => {
+    showButton()
+  }, []);
+
   window.addEventListener('resize', showButton);
 
   return (
     <>
       <nav className='navbar'>
         <div className="navbar-container">
-          <Link to='/' className='navbar-logo'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             BEE <i className='fab fa-typo3' />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
